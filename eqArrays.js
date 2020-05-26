03eaa72475026c6120ad163e9e8175b99d3efada
John Saputo <jsaputo1@gmail.com>
2020-05-26 23:11:25 +0000

    eqArrays exercise

---
+++ eqArrays.js
@@ -0,0 +1,24 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`👍 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`👎 Assertion Failed:  ${actual} !==  ${expected}`);
+  }
+};
+
+const eqArrays = function (x, y) {
+  if (x.length !== y.length) {
+    return false;
+  }
+  for (let i = 0; i < x.length; i++) {
+    if (x[i] !== y[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
