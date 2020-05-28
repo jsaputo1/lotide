d4147835cd1e5b30e1d63d24e6a6780fc074aef2
John Saputo <jsaputo1@gmail.com>
2020-05-28 00:15:46 +0000

    eqObjects assignment. Compares two objects

---
+++ eqObjects.js
@@ -0,0 +1,33 @@
+const green = "\x1b[32m%s\x1b[0m";
+const red = "\x1b[31m%s\x1b[0m";
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(green, `Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(red, `Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// Returns true if both objects have identical keys with identical values.
+// Otherwise you get back a big fat false!
+const eqObjects = function (object1, object2) {
+  const firstObject = Object.keys(object1);
+  const secondObject = Object.keys(object2);
+  // Storing the keys in variables. These variables are arrays
+  if (firstObject.length != secondObject.length) {
+    return false;
+  }
+  for (let value of firstObject) {
+    if (firstObject[value] !== secondObject[value]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+assertEqual(eqObjects(ab, ba), true); // => true
+
+const abc = { a: "1", b: "2", c: "3" };
+assertEqual(eqObjects(ab, abc), false); // => false
