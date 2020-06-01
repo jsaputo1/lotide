981157fc0e9442f1704e4d8886f6efb4a39d85ed
John Saputo <jsaputo1@gmail.com>
2020-06-01 20:07:16 +0000

    refactored code to use mocha/chai as test

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,7 +1,8 @@
+const assert = require("chai").assert;
 const tail = require("../tail");
 
-const assertEqual = require("../assertEqual");
-
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-console.log(tail(words));
-assertEqual(words.length, 3);
+describe("#tail", () => {
+  it("returns [ 2, 3 ] for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
+});
