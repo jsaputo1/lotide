09ffcfcb43c24d6997fcc5a20f04c89ee77db317
John Saputo <jsaputo1@gmail.com>
2020-06-01 17:16:14 +0000

    created test to see if tail.js exported correctly

---
+++ test/tailTest.js
@@ -0,0 +1,7 @@
+const tail = require("../tail");
+
+const assertEqual = require("../assertEqual");
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+console.log(tail(words));
+assertEqual(words.length, 3);
