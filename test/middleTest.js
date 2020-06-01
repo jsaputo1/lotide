d63b36ed508f459184acae9efc372d64cd98c95f
John Saputo <jsaputo1@gmail.com>
2020-06-01 20:13:15 +0000

    refactored code to use mocha/chai as test

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,17 +1,11 @@
 const middle = require("../middle");
-const assertArraysEqual = require("../assertArraysEqual");
+const assert = require("chai").assert;
 
-middle([1]); // => []
-middle([1, 2]); // => []
-
-middle([1, 2, 3]); // => [2]
-middle([1, 2, 3, 4, 5]); // => [3]
-
-middle([1, 2, 3, 4]); // => [2, 3]
-middle([1, 2, 3, 4, 5, 6]); // => [3, 4]
-
-assertArraysEqual(middle([1]), []); // => [] should PASS
-assertArraysEqual(middle([1, 2]), []); // => [] should PASS
-
-assertArraysEqual(middle([1, 2, 3]), [2, 3]); // => [2] // => should FAIL
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3, 4]); // => [3] should FAIL
+describe("#middle", () => {
+  it("returns one middle value of an array if it is odd", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+  it("returns two middle values of an array if it is equal", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+});
