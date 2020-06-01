feab2de82afbeabe0400676c9aacbc4c6459b033
John Saputo <jsaputo1@gmail.com>
2020-06-01 18:35:44 +0000

    replaced colors in response messages with emojis

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,28 +1,12 @@
-const green = "\x1b[32m%s\x1b[0m";
-const red = "\x1b[31m%s\x1b[0m";
-
-const eqArrays = function (x, y) {
-  if (x.length !== y.length) {
-    return false;
-  }
-  for (let i = 0; i < x.length; i++) {
-    if (x[i] !== y[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require("./eqArrays");
 
 const assertArraysEqual = function (x, y) {
   let flag = eqArrays(x, y);
   if (flag === true) {
-    console.log(green, `Assertion Passed: ${x} === ${y}`);
+    console.log(`👍 Assertion Passed: ${x} === ${y}`);
   } else {
-    console.log(red, `Assertion Failed: ${x} !== ${y}`);
+    console.log(` ⛔ Assertion Failed: ${x} !== ${y}`);
   }
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3], true); // => true
-assertArraysEqual([1, 2, 3], [3, 2, 1]), false; // => false
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"], true); // => true
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]), false; // => false
+module.exports = assertArraysEqual;
