a47c9b60e93c94d0b6cedaeaf7c841395316ec27
John Saputo <jsaputo1@gmail.com>
2020-06-01 18:34:55 +0000

    added export module to middle function

--- middle.js
+++ middle.js
@@ -1,26 +1,5 @@
-const green = "\x1b[32m%s\x1b[0m";
-const red = "\x1b[31m%s\x1b[0m";
-
-const eqArrays = function (x, y) {
-  if (x.length !== y.length) {
-    return false;
-  }
-  for (let i = 0; i < x.length; i++) {
-    if (x[i] !== y[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function (x, y) {
-  let flag = eqArrays(x, y);
-  if (flag === true) {
-    console.log(green, `Assertion Passed: ${x} === ${y}`);
-  } else {
-    console.log(red, `Assertion Failed: ${x} !== ${y}`);
-  }
-};
+const eqArrays = require("./eqArrays");
+const assertArraysEqual = require("./assertArraysEqual");
 
 const middle = function (array) {
   let newArray = [];
@@ -38,17 +17,4 @@ const middle = function (array) {
   return newArray;
 };
 
-middle([1]); // => []
-middle([1, 2]); // => []
-
-middle([1, 2, 3]); // => [2]
-middle([1, 2, 3, 4, 5]); // => [3]
-
-middle([1, 2, 3, 4]); // => [2, 3]
-middle([1, 2, 3, 4, 5, 6]); // => [3, 4]
-
-assertArraysEqual(middle([1]), []); // => [] should PASS
-assertArraysEqual(middle([1, 2]), []); // => [] should PASS
-
-assertArraysEqual(middle([1, 2, 3]), [2, 3]); // => [2] // => should FAIL
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3, 4]); // => [3] should FAIL
+module.exports = middle;
