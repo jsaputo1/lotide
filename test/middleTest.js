d2ea3e63c26e5f49c1881a6e9162e5527f3bb1ef
John Saputo <jsaputo1@gmail.com>
2020-06-01 20:15:13 +0000

    removed comments

--- test/middleTest.js
+++ test/middleTest.js
@@ -15,18 +15,3 @@ describe("#middle", () => {
     assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
   });
 });
-
-// middle([1]); // => []
-// middle([1, 2]); // => []
-
-// middle([1, 2, 3]); // => [2]
-// middle([1, 2, 3, 4, 5]); // => [3]
-
-// middle([1, 2, 3, 4]); // => [2, 3]
-// middle([1, 2, 3, 4, 5, 6]); // => [3, 4]
-
-// assertArraysEqual(middle([1]), []); // => [] should PASS
-// assertArraysEqual(middle([1, 2]), []); // => [] should PASS
-
-// assertArraysEqual(middle([1, 2, 3]), [2, 3]); // => [2] // => should FAIL
-// assertArraysEqual(middle([1, 2, 3, 4, 5]), [3, 4]); // => [3] should FAIL
