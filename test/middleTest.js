46afeddd83fcea7bda6f92f9265a71cb164a18fd
John Saputo <jsaputo1@gmail.com>
2020-06-01 20:14:46 +0000

    added more test examples

--- test/middleTest.js
+++ test/middleTest.js
@@ -8,4 +8,25 @@ describe("#middle", () => {
   it("returns two middle values of an array if it is equal", () => {
     assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
   });
+  it("returns one middle value of an array if it is odd", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
+  it("returns two middle values of an array if it is equal", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  });
 });
+
+// middle([1]); // => []
+// middle([1, 2]); // => []
+
+// middle([1, 2, 3]); // => [2]
+// middle([1, 2, 3, 4, 5]); // => [3]
+
+// middle([1, 2, 3, 4]); // => [2, 3]
+// middle([1, 2, 3, 4, 5, 6]); // => [3, 4]
+
+// assertArraysEqual(middle([1]), []); // => [] should PASS
+// assertArraysEqual(middle([1, 2]), []); // => [] should PASS
+
+// assertArraysEqual(middle([1, 2, 3]), [2, 3]); // => [2] // => should FAIL
+// assertArraysEqual(middle([1, 2, 3, 4, 5]), [3, 4]); // => [3] should FAIL
