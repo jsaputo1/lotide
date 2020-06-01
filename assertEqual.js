987f06f0d48559d55991c6cef61added13c94ae6
John Saputo <jsaputo1@gmail.com>
2020-06-01 22:01:13 +0000

    removed comments

--- assertEqual.js
+++ assertEqual.js
@@ -1,18 +1,9 @@
 const assertEqual = function (actual, expected) {
   if (actual === expected) {
-    console.log(` 👍  Assertion Passed: ${actual} === ${expected}`);
+    console.log(green, `👍  Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log(` ⛔ Assertion Failed: ${actual} !== ${expected}`);
+    console.log(red, ` ⛔ Assertion Failed: ${actual} !== ${expected}`);
   }
 };
-
-// TEST CODE
-// assertEqual("Lighthouse Labs", "Bootcamp");
-// assertEqual(1, 1);
-
-// assertEqual.red = red;
-// assertEqual.green = green;
-
-// module.exports = { red, green };
 
 module.exports = assertEqual;
